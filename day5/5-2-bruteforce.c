19e90c31f262250a95e5f36fbbdf0aeb22f5fa30
laptop <noamtsalisher@yahoo.com>
2024-08-23 12:22:07 +0300

    remove .out files, done with day 1 part 1, added day2 part bruteforce approach

--- day5/5-2-bruteforce.c
+++ day5/5-2-bruteforce.c
@@ -15,9 +15,8 @@ typedef struct Map {
 	char is_mapped;
 } Map;
 
-Map *set_seeds(char *line, int num_of_seeds);
-int count_seeds(char *line);
-void print_arr(Map *arr, int arr_len);
+Map *set_seeds(char **line, long unsigned *arr_len);
+void print_arr(Map *arr, long unsigned arr_len);
 void map_seeds(FILE *fp, Map *arr, int arr_len);
 void map_seeds_from_line(FILE *fp, Map *arr, int arr_len, char *line_start);
 void read_numbers(char *line, long unsigned *first, long unsigned *second, long unsigned *third);
@@ -27,115 +26,97 @@ int main()
 {
 	char line[MAX_LINE_LENGTH + 1], *ptr;
 	Map *seed_arr;
-	int seeds_num;
-	long unsigned min_location_mapping;
+	long unsigned min_location_mapping, curr_min;
+	long unsigned seeds_num = 0;
 	FILE *fp = fopen("almanac.txt", "r");
 	if (!fp)
 		return 1;
 	while (fgets(line, MAX_LINE_LENGTH, fp) && ((ptr = strstr(line, "seeds: ")) == NULL))
-			; /* get seeds ptr into seeds_ptr */
+			;
 	if (!ptr || !fp)
 		return 1; /* failed */
 	ptr += strlen("seeds: ");
-	seeds_num = count_seeds(ptr);
-	printf("seeds num: %d\n", seeds_num);
-	seed_arr = set_seeds(ptr, seeds_num);
+	printf("ptr is %s\n", ptr);
+	seed_arr = set_seeds(&ptr, &seeds_num);
+	printf("seeds number = %lu\n", seeds_num);
 	printf("seeds as arr:\n");
-	print_arr(seed_arr, seeds_num);
+	if (!fp)
+		fp = fopen("alamanac.txt", "r");
 	map_seeds(fp, seed_arr, seeds_num);
+	fclose(fp);
 	min_location_mapping = get_minimum(seed_arr, seeds_num);
-	printf("minimum location number is %lu\n", min_location_mapping);
+
 	free(seed_arr);
+	while (*ptr)
+	{
+
+		printf("ptr is %s\n", ptr);
+		seed_arr = set_seeds(&ptr, &seeds_num);
+		printf("seeds number = %lu\n", seeds_num);
+
+		if (!fp)
+			fp = fopen("alamanac.txt", "r");
+		map_seeds(fp, seed_arr, seeds_num);
+		fclose(fp);
+		curr_min = get_minimum(seed_arr, seeds_num);
+		min_location_mapping = min_location_mapping < curr_min ? min_location_mapping : curr_min;
+		free(seed_arr);
+	}
+	printf("\nminimum location number is %lu\n", min_location_mapping);
 	return 0;
 }
 
-void print_arr(Map *arr, int arr_len)
+void print_arr(Map *arr, long unsigned arr_len)
 {
+	if (!arr)
+		return;
 	int i;
 	for (i = 0; i < arr_len; ++i)
 		printf("arr[%d]: val = %lu map = %lu\n", i, arr[i].val, arr[i].mapped_val);
 }
 
-int count_seeds(char *line)
-{
-	int i = 0;
-	while (*line)
-	{
-		while (*line == ' ' || *line == '\t' || *line == '\t')
-			++line;
-		if (isdigit(*line))
-			++i;
-		while (isdigit(*line++))
-			;
-	}	
-	return i;
-}
-
-Map *set_seeds(char *line, int num_of_seeds)
+Map *set_seeds(char **line, long unsigned *arr_len)
 {
 	Map *arr;
-	int i;
-	arr = (Map *)calloc(num_of_seeds, sizeof(Map));
-	i = 0;
-	while (*line)
-	{
-		while (*line == ' ' || *line == '\t' || *line == '\t')
-			++line;
-		while (isdigit(*line))
-			arr[i].val = (arr[i].val) * 10 + *line++ - '0';
-		arr[i].mapped_val = arr[i].val; /* default to map value to the same value */
-		++line, ++i;
-	}	
-	return arr;
-}
+	unsigned long start_range, range, i;
+	start_range = range = 0;
 
-List *set_list(char *line)
-{
-	List *head, *ptr;
-	while (*line == ' ' || *line == '\t')
-		++line;
-	if (isdigit(*line))
-		head = (List *)malloc(sizeof(List));
-	else
+	while (**line && (**line == ' ' || **line == '\t' || **line == '\n'))
+		++(*line);
+	if (!isdigit(**line))
 	{
 		printf("Not an integer\n");
 		return NULL;
 	}
-
-	head->val = 0;
-	while (isdigit(*line))
-		head->val = (head->val) * 10 + *line++ - '0';
-
-	ptr = head;
-	while (*line)
+	while (isdigit(**line))
 	{
-		while (*line == ' ' || *line == '\t' || *line == '\n')
-			++line;
-		if (isdigit(*line))
-			ptr->next = (List *)malloc(sizeof(List));
-		else
-		{
-			printf("Not an integer\n");
-			return NULL;
-		}
-
-		ptr = ptr->next;
-		ptr->val = 0;
-		while (isdigit(*line))
-			ptr->val = (ptr->val) * 10 + *line++ - '0';
-		++line;
+		start_range = start_range * 10 + **line - '0';
+		++(*line);
 	}
-	return head;
-}
 
-void print_list(List *list)
-{
-	int i = 0;
-	while (list)
+	while (**line && (**line == ' ' || **line == '\t' || **line == '\n'))
+		++(*line);
+	if (!isdigit(**line))
 	{
-		printf("val %d = %d\n", ++i, list->val);
-		list = list->next;
+		printf("Not an integer\n");
+		return NULL;
 	}
+	while (isdigit(**line))
+	{
+		range = range * 10 + **line - '0';
+		++(*line);
+	}
+
+	while (**line && (**line == ' ' || **line == '\t' || **line == '\n'))
+		++(*line);
+
+	printf("start %lu range %lu\n", start_range, range);
+	arr = (Map *)malloc(sizeof(Map) * range);
+	for (i = 0; i < range; ++i) /* set from end of array, x = range cells */
+		arr[i].val = arr[i].mapped_val  = start_range + i;
+
+	*arr_len = range;
+	return arr;
 }
 
 void map_seeds(FILE *fp, Map *arr, int arr_len)
@@ -144,37 +125,30 @@ void map_seeds(FILE *fp, Map *arr, int arr_len)
 	map_seeds_from_line(fp, arr, arr_len, "seed-to-soil map:");
 	if (!fp)
 		return; /* failed, we expect more lines*/
-	print_arr(arr, arr_len);
 	printf("\nmap soil to fertilizer\n");
 	map_seeds_from_line(fp, arr, arr_len, "soil-to-fertilizer map:");
 	if (!fp)
 		return; /* failed, we expect more lines*/
-	print_arr(arr, arr_len);
 	printf("\nmap fertilizer to water\n");
 	map_seeds_from_line(fp, arr, arr_len, "fertilizer-to-water map:");
 	if (!fp)
 		return; /* failed, we expect more lines*/
-	print_arr(arr, arr_len);
 	printf("\nmap water to light\n");
 	map_seeds_from_line(fp, arr, arr_len, "water-to-light map:");
 	if (!fp)
 		return; /* failed, we expect more lines*/
-	print_arr(arr, arr_len);
 	printf("\nmap light to termperature\n");
 	map_seeds_from_line(fp, arr, arr_len, "light-to-temperature map:");
 	if (!fp)
 		return; /* failed, we expect more lines*/
-	print_arr(arr, arr_len);
 	printf("\nmap termperature to humidity\n");
 	map_seeds_from_line(fp, arr, arr_len, "temperature-to-humidity map:");
 	if (!fp)
 		return; /* failed, we expect more lines*/
-	print_arr(arr, arr_len);
 	printf("\nmap humidity to location\n");
 	map_seeds_from_line(fp, arr, arr_len, "humidity-to-location map:");
 	if (!fp)
 		return; /* failed, we expect more lines*/
-	print_arr(arr, arr_len);
 }
 
 void map_seeds_from_line(FILE *fp, Map *arr, int arr_len, char *line_search)
